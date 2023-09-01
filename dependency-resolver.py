3cc0bea870ffc5923bd37e9338b324e75a63d98b
ledan87 <daniel.dyk87@gmail.com>
2023-09-01 10:48:08 +0000

    Add ssh-agent and ssh-slaves

--- dependency-resolver.py
+++ dependency-resolver.py
@@ -1,17 +1,21 @@
 import zipfile
-from io import BytesIO
-from urllib.request import urlopen
-from dependency import Dependency, getRecursiveDependencies
 
-dependencies = []
-dependencies.append(Dependency("git", "4.11.5", "2.289.1"))
-dependencies.append(Dependency("msbuild", "1.30", "2.289.1"))
-dependencies.append(Dependency("workflow-aggregator", "590.v6a_d052e5a_a_b_5", "2.303.3"))
+from dependency import Dependency, get_manifest, get_recursive_dependencies
 
-all_deps = getRecursiveDependencies(dependencies)
+dependencies = [
+    Dependency("git", "4.11.5", "2.289.1"),
+    Dependency("msbuild", "1.30", "2.289.1"),
+    Dependency("workflow-aggregator", "590.v6a_d052e5a_a_b_5", "2.303.3"),
+    Dependency("ssh-agent", "333.v878b_53c89511", "2.332.4"),
+    Dependency("ssh-slaves", "2.877.v365f5eb_a_b_eec", "2.361.4"),
+    Dependency("http_request", "1.18", "2.361.4"),
+    Dependency("javadoc", "226.v71211feb_e7e9", "2.361.4"),
+]
+
+all_deps = get_recursive_dependencies(dependencies)
 
 # get latest version of each plugin
-latest_deps = {}
+latest_deps: dict[str, Dependency] = {}
 for dep in all_deps:
     if dep.name not in latest_deps or dep.version > latest_deps[dep.name].version:
         latest_deps[dep.name] = dep
@@ -19,17 +23,30 @@ for dep in all_deps:
 # get highest jenkins version of all latest plugins
 highest_jenkins_version = "0"
 for dep in latest_deps.values():
-    if dep.requiredJenkinsVersion > highest_jenkins_version:
-        highest_jenkins_version = dep.requiredJenkinsVersion
+    if dep.required_jenkins_version > highest_jenkins_version:
+        highest_jenkins_version = dep.required_jenkins_version
 
-#print highest jenkins version
-print(f"Highest Jenkins Version: {highest_jenkins_version}")
+# print highest jenkins version
+print(f"Highest Jenkins Version: {highest_jenkins_version}")  # noqa: T201
 
-#print latest dependencies with version and required jenkins version
+# print latest dependencies with version and required jenkins version
 for dep in latest_deps.values():
-    print(f"{dep.name}:{dep.version} (Jenkins Version: {dep.requiredJenkinsVersion})")
+    manifest = get_manifest(dep)
+    try:
+        if "Long-Name" in manifest:
+            print(f"{manifest['Long-Name']}; {dep.version}")  # noqa: T201
+        # elif "Implementation-Title" in manifest:
+        #     print(f"{manifest['Implementation-Title']}; {dep.version}")  # noqa: T201
+        else:
+            if "Long-Name: JavaScript GUI Lib" in manifest:
+                name = f"JavaScript GUI Lib {manifest['Long-Name: JavaScript GUI Lib']}"
+            else:
+                name = f"Pipeline: {manifest['Long-Name: Pipeline']}"
+            print(f"{name}; {dep.version}")  # noqa: T201
+    except KeyError:
+        print(f"{dep.name}; {dep.version}")
 
 with zipfile.ZipFile("jenkins-plugins.zip", "w") as zip:
     for hpi in latest_deps.values():
-        #write hpi to zip with name from file_name
-        zip.write(hpi.file_name(), arcname = f"{hpi.name}.hpi")
+        # write hpi to zip with name from file_name
+        zip.write(hpi.file_name(), arcname=f"{hpi.name}.hpi")
--- dependency.py
+++ dependency.py
@@ -1,28 +1,50 @@
-import os.path
+# ruff: noqa: D101, D102, D103, ANN401
+
+"""Dependency Resolver."""
+
+from __future__ import annotations
+
+import logging
 import re
 import zipfile
+from pathlib import Path
+from typing import IO, Any
 from urllib.request import urlopen
 
 MANIFEST_CONTENT_PATTERN = re.compile("(?P<name>.+): (?P<content>.*)")
 
+
 class Dependency:
-    def __init__(self, name, version, requiredJenkinsVersion):
+    """Dependency class."""
+
+    def __init__(
+        self: Dependency, name: str, version: str, required_jenkins_version: str
+    ) -> None:
         self.name = name
         self.version = version
-        self.requiredJenkinsVersion = requiredJenkinsVersion
+        self.required_jenkins_version = required_jenkins_version
+        self.logger = logging.getLogger(__name__)
 
-    def download_link(self):
-        print(f"Downloading {self.name} {self.version}")
-        return f"https://updates.jenkins.io/download/plugins/{self.name}/{self.version}/{self.name}.hpi"
+    def download_link(self: Dependency) -> str:
+        self.logger.info(f"Downloading {self.name} {self.version}")
+        return (
+            "https://updates.jenkins.io/download/plugins/"
+            f"{self.name}/{self.version}/{self.name}.hpi"
+        )
 
-    def file_name(self):
+    def file_name(self: Dependency) -> str:
         return f"cache/{self.name}_{self.version}.hpi"
 
-    def __str__(self):
+    def __str__(self: Dependency) -> str:
         return f"{self.name}:{self.version}"
 
-def parse_manifest(manifest):
-    lines = [line for line in manifest.read().decode("utf-8").splitlines() if line.strip() != ""]
+
+def parse_manifest(manifest: IO[bytes]) -> Any:
+    lines = [
+        line
+        for line in manifest.read().decode("utf-8").splitlines()
+        if line.strip() != ""
+    ]
 
     contents = {}
     lastname = ""
@@ -37,56 +59,66 @@ def parse_manifest(manifest):
     return contents
 
 
-def getManifest(dep):
-    if not os.path.exists("cache"):
-        os.makedirs("cache")
+def get_manifest(dep: Dependency) -> Any:
+    cache_path = Path("cache")
+    cache_path.mkdir(parents=True, exist_ok=True)
 
     file_name = dep.file_name()
-    if not os.path.exists(file_name):
+    if not Path(file_name).exists():
         url = dep.download_link()
-        with urlopen(url) as response:
-            open(file_name, "wb").write(response.read())
+        with urlopen(url) as response, Path(file_name).open("wb") as f:
+            f.write(response.read())
 
-    with open(file_name, "rb") as file:
-        with zipfile.ZipFile(file_name) as zip:
-            return parse_manifest(zip.open("META-INF/MANIFEST.MF"))
+    with zipfile.ZipFile(file_name) as zip:
+        return parse_manifest(zip.open("META-INF/MANIFEST.MF"))
 
-def getDependencies(manifest):
+
+def get_dependencies(manifest: dict[str, str]) -> list[Dependency]:
     deps = []
     if "Plugin-Dependencies" in manifest:
         for dep in manifest["Plugin-Dependencies"].split(","):
             if ";" in dep:
                 dep = dep.split(";")[0]
             name, version = dep.split(":")
-            requiredJenkinsVersion = getJenkinsVersion(manifest)
-            deps.append(Dependency(name, version, requiredJenkinsVersion))
+            required_jenkins_version = get_jenkins_version(manifest)
+            deps.append(Dependency(name, version, required_jenkins_version))
     return deps
 
-def getJenkinsVersion(manifest):
+
+def get_jenkins_version(manifest: dict[str, str]) -> str:
     return manifest["Jenkins-Version"]
 
-# get recursive dependencies in a list and cache already resolved dependencies to avoid infinite loops
-def getRecursiveDependenciesCached(dependencies, cache):
+
+def get_recursive_dependencies_cached(
+    dependencies: list[Dependency], cache: dict[Dependency, str]
+) -> list[Dependency]:
+    """Get recursive dependencies in a list.
+
+    Cache already resolved dependencies to avoid infinite loops.
+    """
     deps = []
     for dep in dependencies:
         if dep in cache:
             continue
-        
-        # if dependency in cache has lower version than the one we want to resolve, we need to resolve it again
+
+        # if dependency in cache has lower version than the one we want to resolve, we
+        # need to resolve it again
         if dep in cache and cache[dep] < dep.version:
             del cache[dep]
 
-        cache.append(dep)
-        manifest = getManifest(dep)
+        cache[dep] = dep.version
+        manifest = get_manifest(dep)
         deps.append(dep)
-        deps.extend(getRecursiveDependenciesCached(getDependencies(manifest), cache))
+        deps.extend(
+            get_recursive_dependencies_cached(get_dependencies(manifest), cache)
+        )
     return deps
 
-def getRecursiveDependencies(dependencies):
+
+def get_recursive_dependencies(dependencies: list[Dependency]) -> list[Dependency]:
     deps = []
     for dep in dependencies:
-        manifest = getManifest(dep)
+        manifest = get_manifest(dep)
         deps.append(dep)
-        deps.extend(getRecursiveDependencies(getDependencies(manifest)))
+        deps.extend(get_recursive_dependencies(get_dependencies(manifest)))
     return deps
-
