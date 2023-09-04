02418f93e99bdfaef5d4137e73cb21ed1619249f
ledan87 <daniel.dyk87@gmail.com>
2023-09-04 07:22:24 +0000

    Refactor dependency resolver update git, cmakebuilder

--- dependency-resolver.py
+++ dependency-resolver.py
@@ -1,75 +1,71 @@
+# ruff: noqa: T201
 import zipfile
 
-from dependency import Dependency, get_manifest, get_recursive_dependencies
+from dependency import Dependency, parse_version
 
 dependencies = [
-    Dependency("git", "5.0.2", "2.289.1"),  # git integration
-    Dependency("msbuild", "1.30", "2.289.1"),
-    Dependency("mstest", "1.0.0", "2.289.1"),
-    Dependency("http_request", "1.18", "2.361.4"),
-    Dependency("ssh-agent", "333.v878b_53c89511", "2.332.4"),
-    Dependency("ssh-slaves", "2.877.v365f5eb_a_b_eec", "2.361.4"),
-    Dependency("cmakebuilder", "4.1.1", "2.361.4"),  # cmake
-    Dependency("subversion", "2.16.0", "2.361.4"),  # svn
-    Dependency("xunit", "2.3.9", "2.289.1"),  # xunit
-    Dependency("powershell", "1.4", "2.289.1"),
-    Dependency("pipeline-stage-view", "2.33", "2.361.4"),
+    Dependency.create("git", "5.0.2"),  # git integration
+    Dependency.create("msbuild", "1.30"),
+    Dependency.create("mstest", "1.0.0"),
+    Dependency.create("http_request", "1.18"),
+    Dependency.create("ssh-agent", "333.v878b_53c89511"),
+    Dependency.create("ssh-slaves", "2.877.v365f5eb_a_b_eec"),
+    Dependency.create("cmakebuilder", "4.1.1"),  # cmake
+    Dependency.create("subversion", "2.16.0"),  # svn
+    Dependency.create("xunit", "2.3.9"),  # xunit
+    Dependency.create("powershell", "1.4"),
+    Dependency.create("pipeline-stage-view", "2.33"),
     # from here we have some dependencies that have been present
-    Dependency("workflow-aggregator", "590.v6a_d052e5a_a_b_5", "2.303.3"),
-    Dependency("javadoc", "226.v71211feb_e7e9", "2.361.4"),
-    Dependency("conditional-buildstep", "1.4.2", "2.361.4"),
-    Dependency("jenkins-multijob-plugin", "1.31", "2.289.1"),
-    Dependency("envinject-api", "1.199.v3ce31253ed13", "2.332.1"),
-    Dependency("build-pipeline-plugin", "1.5.8", "1.619"),
-    Dependency("dashboard-view", "2.9.11", "2.289.1"),
-    Dependency("antisamy-markup-formatter", "1.1", "2.289.1"),
-    Dependency("throttle-concurrents", "2.6", "2.289.1"),
-    Dependency("sshd", "3.249.v2dc2ea_416e33", "2.289.1"),
-    Dependency("ssh", "2.6.1", "2.289.1"),
-    Dependency("windows-slaves", "1.8.1", "2.289.1"),
-    Dependency("matrix-auth", "2.6.6", "2.289.1"),
-    Dependency("command-launcher", "84.v4a_97f2027398", "2.289.1"),
+    Dependency.create("workflow-aggregator", "590.v6a_d052e5a_a_b_5"),
+    Dependency.create("jenkins-multijob-plugin", "1.31"),
+    Dependency.create("envinject-api", "1.199.v3ce31253ed13"),
+    Dependency.create("build-pipeline-plugin", "1.5.8"),
+    Dependency.create("antisamy-markup-formatter", "1.1"),
+    Dependency.create("throttle-concurrents", "2.6"),
+    Dependency.create("sshd", "3.249.v2dc2ea_416e33"),
+    Dependency.create("ssh", "2.6.1"),
+    Dependency.create("windows-slaves", "1.8.1"),
+    Dependency.create("matrix-auth", "2.6.6"),
+    Dependency.create("command-launcher", "84.v4a_97f2027398"),
 ]
 
-all_deps = get_recursive_dependencies(dependencies)
 
 # get latest version of each plugin
 latest_deps: dict[str, Dependency] = {}
-for dep in all_deps:
-    if dep.name not in latest_deps or dep.version > latest_deps[dep.name].version:
-        latest_deps[dep.name] = dep
+for d in dependencies:
+    for dep in [d, *d.all_dependencies]:
+        if dep.name not in latest_deps or parse_version(dep.version) > parse_version(
+            latest_deps[dep.name].version
+        ):
+            if dep.name in latest_deps:
+                print(  # noqa: T201
+                    f"Found new version of {dep.name}: {parse_version(dep.version)} "
+                    f"was {parse_version(latest_deps[dep.name].version)}"
+                )
+            latest_deps[dep.name] = dep
 
 # get highest jenkins version of all latest plugins
-highest_jenkins_version = "0"
+highest_jenkins_version = parse_version("0")
 for dep in latest_deps.values():
-    if dep.required_jenkins_version > highest_jenkins_version:
-        highest_jenkins_version = dep.required_jenkins_version
+    current_jenkins_version = parse_version(dep.required_jenkins_version)
+    if current_jenkins_version > highest_jenkins_version:
+        highest_jenkins_version = current_jenkins_version
 
 # print highest jenkins version
-print(f"Highest Jenkins Version: {highest_jenkins_version}")  # noqa: T201
+print(f"Highest Jenkins Version: {highest_jenkins_version}")
+
+
+for dep in dependencies:
+    print()
+    print("Dependencies of " + dep.name)  # noqa:
+    dep.print_tree(latest_deps)
 
 # print latest dependencies with version and required jenkins version
 for dep in latest_deps.values():
-    manifest = get_manifest(dep)
-    try:
-        if "Long-Name" in manifest:
-            name = manifest["Long-Name"]
-        else:
-            if "Long-Name: JavaScript GUI Lib" in manifest:
-                name = (
-                    f"JavaScript GUI Lib: {manifest['Long-Name: JavaScript GUI Lib']}"
-                )
-            else:
-                name = f"Pipeline: {manifest['Long-Name: Pipeline']}"
-    except KeyError:
-        name = dep.name
-    if name.startswith("Jenkins "):
-        name = name[8:]
-    print(f"{name}; {dep.version}")  # noqa: T201
-
+    print(f"{dep.long_name}; {dep.version}")
 
 
 with zipfile.ZipFile("jenkins-plugins.zip", "w") as zip:
     for hpi in latest_deps.values():
         # write hpi to zip with name from file_name
-        zip.write(hpi.file_name(), arcname=f"{hpi.name}.hpi")
+        zip.write("cache/" + hpi.file_name, arcname=f"{hpi.name}.hpi")
--- dependency.py
+++ dependency.py
@@ -4,89 +4,150 @@
 
 from __future__ import annotations
 
-import logging
 import re
 import zipfile
 from pathlib import Path
-from typing import IO, Any
+from typing import IO
 from urllib.request import urlopen
 
+from packaging import version
+
 MANIFEST_CONTENT_PATTERN = re.compile("(?P<name>.+): (?P<content>.*)")
+
+__cached__: dict[str, Dependency] = {}
 
 
 class Dependency:
     """Dependency class."""
 
-    def __init__(
-        self: Dependency, name: str, version: str, required_jenkins_version: str
-    ) -> None:
+    def __init__(self: Dependency, name: str, version: str) -> None:
         self.name = name
         self.version = version
-        self.required_jenkins_version = required_jenkins_version
-        self.logger = logging.getLogger(__name__)
+        self.manifest = self.__load_manifest()
+        self.dependencies: list[Dependency] = self.__load_dependencies()
+        self.__visited = False
 
+    @classmethod
+    def create(cls: type[Dependency], name: str, version: str) -> Dependency:
+        id = f"{name}"
+        if id in __cached__:
+            dep = __cached__[id]
+            if parse_version(version) > parse_version(dep.version):
+                print(  # noqa: T201
+                    f"Found a new version of {dep.name}: {version} was {dep.version}"
+                )
+                dep.version = version
+            return dep
+        else:
+            dep = cls(name, version)
+            __cached__[id] = dep
+            return dep
+
+    @property
     def download_link(self: Dependency) -> str:
         return (
             "https://updates.jenkins.io/download/plugins/"
             f"{self.name}/{self.version}/{self.name}.hpi"
         )
 
+    @property
     def file_name(self: Dependency) -> str:
-        return f"cache/{self.name}_{self.version}.hpi"
+        return f"{self.name}_{self.version}.hpi"
+
+    @property
+    def long_name(self: Dependency) -> str:
+        try:
+            if "Long-Name" in self.manifest:
+                name = self.manifest["Long-Name"]
+            else:
+                if "Long-Name: JavaScript GUI Lib" in self.manifest:
+                    name = f"JavaScript GUI Lib: {self.manifest['Long-Name: JavaScript GUI Lib']}"
+                else:
+                    name = f"Pipeline: {self.manifest['Long-Name: Pipeline']}"
+        except KeyError:
+            name = self.name
+        if name.startswith("Jenkins "):
+            name = name[8:]
+        return name
+
+    @property
+    def required_jenkins_version(self: Dependency) -> str:
+        if "Jenkins-Version" not in self.manifest:
+            return "0"
+        return self.manifest["Jenkins-Version"]
+
+    @property
+    def all_dependencies(self: Dependency) -> list[Dependency]:
+        deps = []
+        for dep in self.dependencies:
+            deps.append(dep)
+            deps.extend(dep.all_dependencies)
+        return deps
 
     def __str__(self: Dependency) -> str:
         return f"{self.name}:{self.version}"
 
+    def __load_manifest(self: Dependency) -> dict[str, str]:
+        cache_path = Path("cache")
+        cache_path.mkdir(parents=True, exist_ok=True)
 
-def parse_manifest(manifest: IO[bytes]) -> Any:
-    lines = [
-        line
-        for line in manifest.read().decode("utf-8").splitlines()
-        if line.strip() != ""
-    ]
+        file_name = cache_path / self.file_name
+        if not file_name.exists():
+            url = self.download_link
+            print(f'Downloading {self.name} {self.version} from "{url}"')  # noqa: T201
+            with urlopen(url) as response, Path(file_name).open("wb") as f:
+                f.write(response.read())
 
-    contents = {}
-    lastname = ""
+        with zipfile.ZipFile(file_name) as zip:
+            return self.__parse_manifest(zip.open("META-INF/MANIFEST.MF"))
 
-    for line in lines:
-        m = MANIFEST_CONTENT_PATTERN.match(line)
-        if m is not None:
-            lastname = m.group("name")
-            contents[lastname] = m.group("content").strip()
+    def __parse_manifest(self: Dependency, manifest: IO[bytes]) -> dict[str, str]:
+        """Parse manifest file.
+
+        Parse the manifest file and provide all entries as dict.
+        """
+        lines = [
+            line
+            for line in manifest.read().decode("utf-8").splitlines()
+            if line.strip() != ""
+        ]
+
+        contents = {}
+        line_name = ""
+
+        for line in lines:
+            m = MANIFEST_CONTENT_PATTERN.match(line)
+            if m is not None:
+                line_name = m.group("name")
+                contents[line_name] = m.group("content").strip()
+            else:
+                contents[line_name] = contents[line_name] + line.strip()
+        return contents
+
+    def __load_dependencies(self: Dependency) -> list[Dependency]:
+        deps = []
+        if "Plugin-Dependencies" in self.manifest:
+            for dep in self.manifest["Plugin-Dependencies"].split(","):
+                if ";" in dep:
+                    dep = dep.split(";")[0]
+                name, version = dep.split(":")
+                deps.append(Dependency.create(name, version))
+        return deps
+
+    def print_tree(
+        self: Dependency, latest_deps: dict[str, Dependency], level: int = 0
+    ) -> None:
+        indent = "  " * level
+        if self.__visited:
+            print(f"{indent}{self.name}:{self.version} (already printed)")
         else:
-            contents[lastname] = contents[lastname] + line.strip()
-    return contents
-
-
-def get_manifest(dep: Dependency) -> Any:
-    cache_path = Path("cache")
-    cache_path.mkdir(parents=True, exist_ok=True)
-
-    file_name = dep.file_name()
-    if not Path(file_name).exists():
-        url = dep.download_link()
-        print(f"Downloading {dep.name} {dep.version}:{url}")  # noqa: T201
-        with urlopen(url) as response, Path(file_name).open("wb") as f:
-            f.write(response.read())
-
-    with zipfile.ZipFile(file_name) as zip:
-        return parse_manifest(zip.open("META-INF/MANIFEST.MF"))
-
-
-def get_dependencies(manifest: dict[str, str]) -> list[Dependency]:
-    deps = []
-    if "Plugin-Dependencies" in manifest:
-        for dep in manifest["Plugin-Dependencies"].split(","):
-            if ";" in dep:
-                dep = dep.split(";")[0]
-            name, version = dep.split(":")
-            required_jenkins_version = get_jenkins_version(manifest)
-            deps.append(Dependency(name, version, required_jenkins_version))
-    return deps
-
-
-def get_jenkins_version(manifest: dict[str, str]) -> str:
-    return manifest["Jenkins-Version"]
+            print(f"{indent}{self.name}:{self.version}")
+            self.__visited = True
+            for dependency in self.dependencies:
+                dependency.print_tree(latest_deps, level + 1)
+        # if level == 0:
+        #     for d in __cached__.values():
+        #         d.__visited = False  # noqa: SLF001
 
 
 def get_recursive_dependencies_cached(
@@ -107,18 +168,22 @@ def get_recursive_dependencies_cached(
             del cache[dep]
 
         cache[dep] = dep.version
-        manifest = get_manifest(dep)
         deps.append(dep)
-        deps.extend(
-            get_recursive_dependencies_cached(get_dependencies(manifest), cache)
-        )
+        deps.extend(get_recursive_dependencies_cached(dep.dependencies, cache))
     return deps
 
 
-def get_recursive_dependencies(dependencies: list[Dependency]) -> list[Dependency]:
-    deps = []
-    for dep in dependencies:
-        manifest = get_manifest(dep)
-        deps.append(dep)
-        deps.extend(get_recursive_dependencies(get_dependencies(manifest)))
-    return deps
+def parse_version(version_str: str) -> version.Version:
+    """Parse version string to version.Version."""
+    try:
+        return version.parse(version_str)
+    except version.InvalidVersion:
+        # version string is not compliant
+        # try to remove the last part of the version string
+        # e.g. "590.v6a_d052e5a_a_b_5" -> "590"
+        # e.g. "1.199.v3ce31253ed13" -> "1.199"
+        match = re.search(r"\D", version_str)
+        if match:
+            return version.parse(version_str[: match.start()])
+        else:
+            raise
